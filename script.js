cefe775fcf88995e5defa40aad134f83b8f27006
John Smith <victor.nossborn@hotmail.com>
2017-07-22 15:52:13 +0000

    Cleaning up some of the javascript

--- script.js
+++ script.js
@@ -3,20 +3,39 @@
 var divAmount = 4;
 var color = "grey";
 
+/*
+    Must fix the hover listener function
+    Stops listening when a new grid is created
+*/
+
 $(document).ready(function() {
-    for(var i = 0; i < divAmount*divAmount; i++) {
-        $(".container").append("<div class='" + i + " block'></div>");
-    }
+    createGrid(divAmount*divAmount);
     
     $(".block").hover(function() {
-        $(this).css("background-color", color)
+        $(this).css("background-color", color);
     });
     
-    divAmount = $("input").val();
+    $(".btn").click(function() {
+        deleteGrid();
+        createGrid(divAmount*divAmount);
+    });
 });
 
 function clear() {
     for(var i = 0; i < divAmount*divAmount; i++) {
-        
+        $("." + i).css("background-color", "black");
     }
+}
+
+function createGrid(size) {
+    for(var i = 0; i < size; i++) {
+        $(".container").append("<div class='" + i + " block'></div>");
+    }
+    
+    $(".block").css("width", 512 / Math.sqrt(size));    
+    $(".block").css("height", 512 / Math.sqrt(size));
+}
+
+function deleteGrid() {
+    $(".block").remove();
 }
