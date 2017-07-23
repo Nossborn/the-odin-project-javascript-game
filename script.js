c43f9594745f8a9c6b0c3cbed0db1aac6e20aa97
John Smith <victor.nossborn@hotmail.com>
2017-07-23 14:43:29 +0000

    Fixed javascript and added a frame

--- script.js
+++ script.js
@@ -3,21 +3,21 @@
 var divAmount = 4;
 var color = "grey";
 
-/*
-    Must fix the hover listener function
-    Stops listening when a new grid is created
-*/
-
 $(document).ready(function() {
     createGrid(divAmount*divAmount);
     
-    $(".block").hover(function() {
-        $(this).css("background-color", color);
-    });
-    
-    $(".btn").click(function() {
+    $(".clr-btn").click(function() {
         deleteGrid();
         createGrid(divAmount*divAmount);
+    });
+    
+    $("input").on("keydown", function(e) {
+        if(e.keyCode == 13) {
+            divAmount = $("input").val();
+            deleteGrid();
+            createGrid(divAmount*divAmount);
+            $(this).blur();
+        }
     });
 });
 
@@ -32,10 +32,15 @@ function createGrid(size) {
         $(".container").append("<div class='" + i + " block'></div>");
     }
     
-    $(".block").css("width", 512 / Math.sqrt(size));    
+    $(".block").css("width", 512 / Math.sqrt(size));
     $(".block").css("height", 512 / Math.sqrt(size));
+    
+    $(".block").hover(function() {
+    $(this).css("background-color", color);
+    });
 }
 
 function deleteGrid() {
     $(".block").remove();
 }
+
